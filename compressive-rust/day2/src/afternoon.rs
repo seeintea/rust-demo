7596ec32fda42d7492bf86f9b3472a4945e7bef1
leviegu <gu.lw@foxmail.com>
2023-11-15 15:49:39 +0800

    feat: compressive-rust day2 afternoon

---
+++ compressive-rust/day2/src/afternoon.rs
@@ -0,0 +1,49 @@
+#![allow(unused_variables, dead_code)]
+
+pub fn prefix_matches(prefix: &str, request_path: &str) -> bool {
+    let mut request_segments = request_path.split('/');
+
+    for prefix_segments in prefix.split('/') {
+      let Some(request_segments) = request_segments.next() else {
+        return false;
+      };
+      if request_segments != prefix_segments && prefix_segments != "*" {
+        return false
+      }
+    }
+
+    true
+}
+
+#[test]
+fn test_matches_without_wildcard() {
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers"));
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers/abc-123"));
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers/abc/books"));
+
+    assert!(!prefix_matches("/v1/publishers", "/v1"));
+    assert!(!prefix_matches("/v1/publishers", "/v1/publishersBooks"));
+    assert!(!prefix_matches("/v1/publishers", "/v1/parent/publishers"));
+}
+
+#[test]
+fn test_matches_with_wildcard() {
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books"
+    ));
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/bar/books"
+    ));
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books/book1"
+    ));
+
+    assert!(!prefix_matches("/v1/publishers/*/books", "/v1/publishers"));
+    assert!(!prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/booksByAuthor"
+    ));
+}
--- compressive-rust/day2/src/main.rs
+++ compressive-rust/day2/src/main.rs
@@ -1,4 +1,5 @@
 mod morning;
+mod afternoon;
 
 fn main() {
     morning::subject_1();
